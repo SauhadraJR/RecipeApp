5c11412c84a01a4040a233fc8fe21ad78baa5b76
Sauhadra Jung Rayamajhi <sauhadra.rayamajhi@gmail.com>
2024-03-11 21:50:39 +0545

    Index.html created

--- recipe/base/views.py
+++ recipe/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
+
+def home(request):
+    recipe_obj = Recipe.objects.all()
+    data = {'recipes':recipe_obj}
+    return render(request,'index.html',context = data)
+
